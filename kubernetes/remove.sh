20096ebb847d2aca9687809638eea092ac59cd47
Geovanny Fajardo <gfajardo@govspend.com>
2020-09-07 00:34:18 -0400

    Implement support for JavaScript in Jupyter Lab.

--- kubernetes/remove.sh
+++ kubernetes/remove.sh
@@ -3,4 +3,4 @@ microk8s kubectl config set-context --current --namespace=ai
 # microk8s kubectl delete -f tensorflow-jupyter-data-persistentvolume.yaml
 # microk8s kubectl delete -f tensorflow-jupyter-data-persistentvolumeclaim.yaml
 microk8s kubectl delete -f tensorflow-jupyter-deployment.yaml
-microk8s kubectl delete tensorflow-jupyter
+microk8s kubectl delete service tensorflow-jupyter
